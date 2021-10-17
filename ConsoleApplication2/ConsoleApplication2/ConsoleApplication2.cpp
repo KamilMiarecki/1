2d118ce46d2dd10b83ae5322645cb093c4d0c8b4
KamilMiarecki <92676433+KamilMiarecki@users.noreply.github.com>
2021-10-17 14:54:55 +0200

    Add files via upload

---
+++ ConsoleApplication2/ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,76 @@
+﻿// ConsoleApplication2.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	//zad1
+	/* int numer_albumu;
+	cout << "wprowadz numer albumu ", cin >> numer_albumu;
+	cout << "w" << numer_albumu << endl;*/
+	
+	//zad2
+	/* int liczba;
+	cout << "wprowadz liczbe ", cin >> liczba;
+	if (liczba % 2 == 0)
+		cout << "parzysta" << endl;
+	else
+		cout << "nieparzysta" << endl;*/
+
+	//zad3
+	 /*float liczba1, liczba2;
+	cout << "wprowadz 2 liczby"; cin >> liczba1, cin >> liczba2;
+
+	if (liczba1 >= liczba2)
+		cout << liczba1 << endl;
+	else
+		cout <<liczba2 << endl;*/
+
+	//zad4
+	/*float liczba1, liczba2, liczba3, liczba4, suma, srednia_arytmetyczna;
+	cout << "wprowadz 4 liczby", cin >> liczba1, cin >> liczba2, cin >> liczba3, cin >> liczba4;
+	suma = liczba1 + liczba2 + liczba3 + liczba4;
+	srednia_arytmetyczna = suma / 4;
+	cout << srednia_arytmetyczna << endl;*/
+
+	//zad 5
+	/*float liczba1, liczba2;
+	int wybor_dzialania;
+	cout << "wprowadz 2 liczby", cin >> liczba1, cin >> liczba2;
+	cout << "1. suma" << endl; cout << "2. roznica" << endl; cout << "3.iloczyn" << endl; cout << "4.iloraz" << endl;
+	cout << "wybierz dzialanie ", cin >> wybor_dzialania;
+	switch (wybor_dzialania)
+	{
+	case 1:
+		cout << "suma: " << liczba1 << "+" << liczba2 << "=" << liczba1 + liczba2 << endl;
+		break;
+	case 2:
+		cout << "roznica: " << liczba1 << "-" << liczba2 << "=" << liczba1 - liczba2 << endl;
+		break;
+	case 3:
+		cout << "iloczyn : " << liczba1 << "*" << liczba2 << "=" << liczba1 * liczba2 << endl;
+		break;
+	case 4:
+		cout << "iloraz: " << liczba1 << "/" << liczba2 << "=" << liczba1 / liczba2 << endl;
+		break;
+	default:
+		cout << "nie ma takiej opcji" << endl;*/
+
+	}
+
+
+
+
+
+
+// Uruchomienie programu: Ctrl + F5 lub menu Debugowanie > Uruchom bez debugowania
+// Debugowanie programu: F5 lub menu Debugowanie > Rozpocznij debugowanie
+
+// Porady dotyczące rozpoczynania pracy:
+//   1. Użyj okna Eksploratora rozwiązań, aby dodać pliki i zarządzać nimi
+//   2. Użyj okna programu Team Explorer, aby nawiązać połączenie z kontrolą źródła
+//   3. Użyj okna Dane wyjściowe, aby sprawdzić dane wyjściowe kompilacji i inne komunikaty
+//   4. Użyj okna Lista błędów, aby zobaczyć błędy
+//   5. Wybierz pozycję Projekt > Dodaj nowy element, aby utworzyć nowe pliki kodu, lub wybierz pozycję Projekt > Dodaj istniejący element, aby dodać istniejące pliku kodu do projektu
+//   6. Aby w przyszłości ponownie otworzyć ten projekt, przejdź do pozycji Plik > Otwórz > Projekt i wybierz plik sln
